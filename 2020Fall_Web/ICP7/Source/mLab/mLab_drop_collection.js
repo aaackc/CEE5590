27911354ba375ce04700bb411827d123cd3ef222
aaackc <54451849+aaackc@users.noreply.github.com>
2020-10-08 14:48:13 -0500

    ICP 7

---
+++ 2020Fall_Web/ICP7/Source/mLab/mLab_drop_collection.js
@@ -0,0 +1,14 @@
+
+
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://lesson7:lesson7@ds159563.mlab.com:59563/aplwebdemo';
+
+MongoClient.connect(url, {useNewUrlParser: true},function(err, db) {
+    if (err) throw err;
+    var dbase = db.db("aplwebdemo");
+    dbase.dropCollection("newCollection", function(err, delOK) {
+        if (err) throw err;
+        if (delOK) console.log("Collection deleted");
+        db.close();
+    });
+});
